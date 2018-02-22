5b266979ebd95b186a7df879d91579aa57cb7703
astur <astur@yandex.ru>
2018-02-22 19:00:40 +0300

    code stub

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = () => {};
+module.exports = (db, {
+    name = 'mq',
+    ttl = 30000,
+    tries = 10,
+} = {}) => {
+    db = (async () => {
+        const _db = await db;
+        // Here will be indexes, cleanups, inits, etc.
+        return _db;
+    })();
+    return {
+        add: async () => {},
+        get: async () => {},
+        ack: async () => {},
+        ping: async () => {},
+    };
+};
