55385584d7c91ac54b2a27ad6d437e3540e52fed
astur <astur@yandex.ru>
2018-02-22 20:07:10 +0300

    'add' function

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ module.exports = (db, {
     ttl = 30000,
     tries = 10,
 } = {}) => {
+    const after = (ttl = 0) => Date.now() + ttl;
     db = (async () => {
         const _db = await db;
         // Here will be indexes, cleanups, inits, etc.
         return _db;
     })();
     return {
-        add: async () => {},
+        add: async items => {
+            const _db = await db;
+            if([null, undefined].includes(items)) return [];
+            items = (Array.isArray(items) ? items : [items])
+                .map(item => ({
+                    data: item,
+                    created: after(),
+                    expires: 0,
+                    tries: 0,
+                }));
+            const result = await _db.collection('mq').insertMany(items);
+            return Object.values(result.insertedIds).map(id => `${id}`);
+        },
         get: async () => {},
         ack: async () => {},
         ping: async () => {},
