34da0b40e37aef7b2ccf2500c0c15e3e03ddffbd
astur <astur@yandex.ru>
2018-02-22 19:11:54 +0300

    test stub

--- test.js
+++ test.js
@@ -1,7 +1,30 @@
 const test = require('ava');
-const m = require('.');
+const mq = require('.');
 
-test('mq-mongo', t => {
-    t.true(true);
-    t.is(m, m);
+const mongo = require('mongodb').MongoClient;
+const mongoString = process.env.MONGO_URI ||
+    'mongodb://localhost:27017/test';
+
+const DB = mongo.connect(mongoString)
+    .then(client => {
+        const db = client.db(mongoString.split('/').pop());
+        db.close = client.close.bind(client);
+        return db;
+    })
+    .catch(e => {
+        console.log(e.message);
+        process.exit(1);
+    });
+
+test.serial('default', async t => {
+    const db = await DB;
+    await db.collection('mq').remove({});
+    await mq(DB);
+    t.is(await db.collection('mq').count(), 0);
+});
+
+test.after(async t => {
+    const db = await DB;
+    // await db.dropCollection('mq');
+    await db.close();
 });
